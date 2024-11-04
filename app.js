9085b66fa0dcc14b3f7c4e6ea6c3b91a47ce9bbd
Hemkaran Raghav <hemkaran.raghav@gmail.com>
2024-11-04 18:01:50 +0530

    Added first challenge

--- app.js
+++ app.js
@@ -16,10 +16,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Treasure hunt home page
-app.get('/coding-marvels', (req, res) => {
-  res.render('treasureHunt');
+// Challenge 1
+app.get('/get-you-started', (req, res) => {
+  res.render('getYouStarted');
 });
+
+app.get('/background.png', (req, res) => {
+  res.render('backgroundFlag');
+});
+
+app.get('/forbidden', (req,res) => {
+  res.status(403);
+  res.render('index');
+})
+
+// Challenge 2 - Anjali, Edit access problem with Private page, "cipher the flag" - Yashank
+// Challenge 3 - Rani, first part of "Add to Cart" system "Find second part"
+// Challenge 4 - Yashank, Script.py with Vignere Cypher
 
 const server = app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
